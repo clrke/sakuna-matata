5077d53c553ae83f744690d07e43eb8ff1530812
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-08-09 07:13:36 +0800

    applied placing markers for reports

--- sakuna_matata/views.py
+++ sakuna_matata/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from api.models import Channel
-
+from api.models import Channel,Message
 
 def home(request, name):
     channels = Channel.objects.all()
-
+    messages = Message.objects.all()
     current_channel = Channel.objects.filter(name=name)
     if current_channel.count():
         current_channel = current_channel[0]
@@ -13,5 +12,6 @@ def home(request, name):
 
     return render(request, 'home.html', {
         'channels': channels,
-        'current_channel': current_channel
+        'current_channel': current_channel,
+        'messages' : messages
     })

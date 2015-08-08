70c62eebd9fbc492b9e794c94f8f6e5ca6bb2c22
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-08-08 19:41:36 +0800

    Add parsing of text message

--- api/v1/views.py
+++ api/v1/views.py
@@ -2,19 +2,46 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, HttpResponse
+from api.models import Message, Channel
+import requests
+from sakuna_matata import settings
 
 
 @csrf_exempt
 def home(request):
     if request.method == 'POST':
-        if request.headers["message_type"] == "incoming":
-            try:
-                #request.headers["message"] do something with the messge here
-                return HttpResponse('Accepted')
-            except Exception:
-                return HttpResponse('Error')
+        message_split = request.POST['message'].split('&|!')
+        mobile_number = request.POST['mobile_number']
+
+        channel_name = message_split[0]
+        latitude = message_split[1]
+        longitude = message_split[2]
+        description = message_split[3]
+        contents = message_split[4]
+
+        channel = Channel.objects.filter(name=channel_name)
+
+        if not channel:
+            channel = Channel(name=channel_name)
+            channel.save()
         else:
-            return HttpResponse('Error')
+            channel = channel[0]
+
+        sentiment = int(requests.get('%s?message=%s' % (settings.SENTIMENT_ANALYSIS_URL, contents)).text)
+
+        message = Message(
+            channel=channel,
+            latitude=float(latitude),
+            longitude=float(longitude),
+            description=description,
+            message=contents,
+            mobile_number=mobile_number,
+            sentiment=sentiment,
+        )
+
+        message.save()
+        return HttpResponse('Accepted')
+
     else:
         User.objects.create_superuser('angelhack', 'angelhack@mailinator.com', 'angelhack2015')
         return JsonResponse(User.objects.all()[0].email, safe=False)
--- sakuna_matata/settings.py
+++ sakuna_matata/settings.py
@@ -103,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SENTIMENT_ANALYSIS_URL = 'http://sakuna-matata.mybluemix.net/analyze'

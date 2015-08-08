8ae910959f6ad87cf04e0b41d0fb55ce53d689b8
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-08-08 18:25:34 +0800

    Temporarily add create super user on api/v1

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, HttpResponse
@@ -7,3 +8,6 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     if request.method == 'POST':
         return HttpResponse('Accepted')
+    else:
+        User.objects.create_superuser('angelhack', 'angelhack@mailinator.com', 'angelhack2015')
+        return JsonResponse(User.objects.all()[0].email, safe=False)

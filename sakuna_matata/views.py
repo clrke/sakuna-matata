04a9307c7fe10bc269c94d46446e82d3d2da0289
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-08-09 09:42:30 +0800

    Add aggregation of messages based on a specified radius

--- sakuna_matata/views.py
+++ sakuna_matata/views.py
@@ -1,17 +1,41 @@
-from django.shortcuts import render, redirect
-from api.models import Channel,Message
+from math import sqrt
+from django.shortcuts import render
+from api.models import Channel, Message
+
+MAX_RADIUS = 0.05
+
+
+def distance(xi, x):
+    cx = (xi[0] - x[0]) ** 2
+    cy = (xi[1] - x[1]) ** 2
+
+    if cx + cy != 0:
+        return sqrt(cx + cy)
+    else:
+        return 0
+
 
 def home(request, name):
     channels = Channel.objects.all()
-    messages = Message.objects.all()
     current_channel = Channel.objects.filter(name=name)
     if current_channel.count():
         current_channel = current_channel[0]
+        messages = current_channel.message_set.all()
     else:
         current_channel = Channel.objects.all()[0]
+        messages = Message.objects.all()
+
+    for message in messages:
+        center = (message.latitude, message.longitude)
+        message.total_sentiment = 0
+        for message2 in messages:
+            if distance((message2.latitude, message2.longitude), center) < MAX_RADIUS:
+                message.total_sentiment += message.sentiment
+
+    messages = sorted(messages, key=lambda m: m.total_sentiment)
 
     return render(request, 'home.html', {
         'channels': channels,
         'current_channel': current_channel,
-        'messages' : messages
+        'messages': messages
     })
